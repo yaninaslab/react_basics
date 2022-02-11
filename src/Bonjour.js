9b1ec8a3d1ddb2d8e647a37f8fa9a994c934e778
yaninaslab <yana.belash@gmail.com>
2022-02-11 11:28:14 -0500

    Added new components in App.js

---
+++ src/Bonjour.js
@@ -0,0 +1,5 @@
+function Bonjour() {
+  return <h2 className="french">Bonjour!</h2>;
+}
+
+export default Bonjour;
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+//import
+//function Hello()
+//export
+
+function Hello() {
+  return <h2>Hello!</h2>;
+}
+
+export default Hello;
